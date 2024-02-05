8975484d2e7b6a003d9bbddbf546c382791db8a9
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-05 10:21:47 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+// import { useState, useEffect, useRef } from "react";
+// import { SearchBar } from "./SearchBar/SearchBar";
+// import { ImageGallery } from "./ImageGallery/ImageGallery";
+
+// import { ErrorMessage } from "./ErrorMessage/ErrorMessage";
+// import { LoadMoreBtn } from "./LoadMoreBtn/LoadMoreBtn";
+// import { Loader } from "./Loader/Loader";
+// import { Toaster } from "react-hot-toast";
+// import { fetchArticles } from "../api";
+
+// export const App = () => {
+//   const [query, setQuery] = useState("");
+//   const [page, setPage] = useState(1);
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   const totalPages = useRef(0);
+
+//   const searchArticles = async (newQuery) => {
+//     setQuery(`${Date.now()}/${newQuery}`);
+//     setPage(1);
+//     setArticles([]);
+//     totalPages.current = 1;
+//   };
+
+//   const handleLoadMore = () => {
+//     setPage(page + 1);
+//   };
+
+//   useEffect(() => {
+//     if (!query) {
+//       return;
+//     }
+
+//     async function fetchData() {
+//       try {
+//         setError(false);
+//         setLoading(true);
+
+//         const { results: fetchedData, total_pages } = await fetchArticles(
+//           query.split("/")[1],
+//           page
+//         );
+//         setArticles((prevArticles) => [...prevArticles, ...fetchedData]);
+
+//         totalPages.current = total_pages;
+//       } catch (error) {
+//         setError(true);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchData();
+//   }, [query, page]);
+
+//   return (
+//     <>
+//       <SearchBar onSearch={searchArticles} />
+//       {error && <ErrorMessage />}
+//       {articles.length > 0 && <ImageGallery items={articles} />}
+//       {loading && <Loader load={loading} />}
+//       {articles.length > 0 && !loading && totalPages.current > page && (
+//         <LoadMoreBtn onClick={handleLoadMore} />
+//       )}
+//       <Toaster position="bottom-center" />
+//     </>
+//   );
+// };
+
+// src/components/App.jsx
+import { Routes, Route, NavLink } from "react-router-dom";
+import clsx from "clsx";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
+import css from "./App.module.css";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
+export const App = () => {
+  return (
+    <div>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={buildLinkClass}>
+          About
+        </NavLink>
+        <NavLink to="/products" className={buildLinkClass}>
+          Products
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App.jsx";
+import { App } from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
